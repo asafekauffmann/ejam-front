b9cd8cd2c08a7ad23cce2376fee84813950f5a55
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2023-11-24 21:55:54 -0300

    modell

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'blue': '#252F3D',
+      'blue-light': '#2C7EF8',
+      'blue-lightness': '#EDF3FD',
+      'green': '#59AE43',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+
+
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+
+      'yellow': '#ffc82c',
+
+    }
+  },
+  plugins: [],
+}
+
